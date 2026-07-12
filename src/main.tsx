58b9d2bc187828dd0e257e2fc5dd00031a1f213d
ageu_dev19 <ageumenezes423@gmail.com>
2026-07-12 17:04:25 -0300

    chore: remove Tempo scaffold and unused Supabase, fix build script

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./providers/theme-provider";
 import { LanguageProvider } from "./providers/language-provider";
 
-import { TempoDevtools } from "tempo-devtools";
-TempoDevtools.init();
-
 const basename = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
